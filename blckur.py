4feeac27610b9eee597292f9b1f74a0f1175d46d
Zachary Huff <zach.huff.386@gmail.com>
2014-12-22 16:25:56 -0500

    Add json_default

--- blckur.py
+++ blckur.py
@@ -27,27 +27,48 @@ class TestCaseReportFilter(object):
         return True
 
 class TestCaseReportFormatter(object):
+    def json_default(self, obj):
+        if hasattr(obj, '__name__'):
+            return '%s()' % obj.__name__
+        raise TypeError(repr(obj) + ' is not JSON serializable')
+
     def error(self, test_case, error_msg):
         return '***************************************************\n' + \
             '%s\n' % error_msg + \
             'name: %r\n' % test_case.__class__.__name__ + \
             'method: %r\n' % test_case.method + \
             'path: %r\n' % test_case.path + \
-            'expect_status: %s\n' % json.dumps(test_case.expect_status,
-                indent=JSON_INDENT) + \
-            'response_status: %s\n' % test_case.response_status + \
-            'expect_headers: %s\n' % json.dumps(test_case.expect_headers,
-                indent=JSON_INDENT) + \
-            'response_headers: %s\n' % json.dumps(
-                dict(test_case.response_headers.items()),
+            'expect_status: %s\n' % json.dumps(
+                test_case.expect_status,
+                default=self.json_default,
                 indent=JSON_INDENT,
             ) + \
-            'input_data: %s\n' % json.dumps(test_case.input_data,
-                indent=JSON_INDENT) + \
-            'expect_data: %s\n' % json.dumps(test_case.expect_data,
-                indent=JSON_INDENT) + \
-            'response_data: %s\n' % json.dumps(test_case.response_data,
-                indent=JSON_INDENT) + \
+            'response_status: %s\n' % test_case.response_status + \
+            'expect_headers: %s\n' % json.dumps(
+                test_case.expect_headers,
+                default=self.json_default,
+                indent=JSON_INDENT,
+            ) + \
+            'response_headers: %s\n' % json.dumps(
+                dict(test_case.response_headers.items()),
+                default=self.json_default,
+                indent=JSON_INDENT,
+            ) + \
+            'input_data: %s\n' % json.dumps(
+                test_case.input_data,
+                default=self.json_default,
+                indent=JSON_INDENT,
+                ) + \
+            'expect_data: %s\n' % json.dumps(
+                test_case.expect_data,
+                default=self.json_default,
+                indent=JSON_INDENT,
+                ) + \
+            'response_data: %s\n' % json.dumps(
+                test_case.response_data,
+                default=self.json_default,
+                indent=JSON_INDENT,
+                ) + \
             '***************************************************'
 
 class TestCaseReportHandler(object):
